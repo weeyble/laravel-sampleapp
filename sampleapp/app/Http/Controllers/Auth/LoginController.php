92062057489f8c37ea7067558de2f6d456faa468
kkb <kkbyu0@gmail.com>
2018-10-11 19:53:33 +0900

    :up: add 1-3-6

--- sampleapp/app/Http/Controllers/Auth/LoginController.php
+++ sampleapp/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,4 +37,13 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->invalidate();
+
+        return redirect('/home');
+    }
+
 }
